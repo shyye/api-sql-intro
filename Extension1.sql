b402fbf81e0248455a091b843b6a160bbb771407
shyye <emma.art.dev@gmail.com>
2024-09-03 13:08:05 +0200

    Add Extension 1 part

---
+++ Extension1.sql
@@ -0,0 +1,10 @@
+-- EXTENSION 1
+-- Return the average film rating
+SELECT AVG(score) FROM films;
+
+-- Return the total number of films
+SELECT COUNT(id) FROM films;
+
+-- Return the average film rating by genre
+SELECT genre, AVG(score) FROM films
+GROUP BY genre;
